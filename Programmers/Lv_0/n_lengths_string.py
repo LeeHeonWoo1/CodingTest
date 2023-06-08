1053fbf9a29c03e4c8a5583e2d1c0634575ec080
LeeHeonWoo1 <gjsdn245@naver.com>
2023-06-08 23:22:37 +0900

    문자열 뒤의 n글자

---
+++ Programmers/Lv_0/n_lengths_string.py
@@ -0,0 +1,22 @@
+"""
+문자열 뒤의 n글자
+
+<문제 설명>
+문자열 my_string과 정수 n이 매개변수로 주어질 때, my_string의 뒤의 n글자로 이루어진 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+<제한 사항>
+my_string은 숫자와 알파벳으로 이루어져 있습니다.
+1 ≤ my_string의 길이 ≤ 1,000
+1 ≤ n ≤ my_string의 길이
+
+<입출력 예시>
+  - 입력:
+    my_string = 'ProgrammerS123', n = 11
+    
+  - 출력:
+    result = grammerS123
+"""
+
+# 내가 작성한 코드
+def solution(my_string, n):
+    return my_string[len(my_string)-n:]
